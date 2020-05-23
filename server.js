25d01400d8eb4c2ecf802d9e064d239dabbc6408
muNeves3 <62778436+muNeves3@users.noreply.github.com>
2020-05-23 15:56:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://muriloNeves:<password>@cluster0-ugije.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://muriloNeves:<murilogrilo2212>@cluster0-ugije.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
